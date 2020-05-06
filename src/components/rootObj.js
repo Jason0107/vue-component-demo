b07d323e7cac41e48ba2071564ccb8db5cc6eec5
yulin <cyl234724@alibaba-inc.com>
2020-05-06 22:12:40 +0800

    update

--- src/components/rootObj.js
+++ src/components/rootObj.js
@@ -1,45 +1,45 @@
-import Vue from 'vue'
-
-
-Vue.component('root-obj', {
-    data() {
-        return {
-            msg: '我是子组件'
+// import Vue from 'vue'
+function plugins(Vue){
+    Vue.component('root-obj', {
+        data() {
+            return {
+                msg: '我是子组件'
+            }
+        },
+        template: `
+            <div>
+                <button @click='getRoot'>子组件</button>
+                <child-component></child-component>
+            </div>`,
+        methods: {
+            getRoot() {
+                console.log(this.msg)
+                console.log(this.$parent.msg)
+                console.log(this.$root.msg)
+            }
         }
-    },
-    template: `
-        <div>
-            <button @click='getRoot'>子组件</button>
-            <child-component></child-component>
-        </div>`,
-    methods: {
-        getRoot() {
-            console.log(this.msg)
-            console.log(this.$parent.msg)
-            console.log(this.$root.msg)
-        }
-    }
 
-})
-Vue.component('child-component', {
-    data() {
-        return {
-            msg: '我是子子组件'
+    })
+    Vue.component('child-component', {
+        data() {
+            return {
+                msg: '我是子子组件'
+            }
+        },
+        template: `
+            <div>
+                <button @click='getRoot'>子子组件</button>
+            </div>`,
+        methods: {
+            getRoot() {
+                console.log(this.msg)
+                console.log(this.$parent.msg)
+                console.log(this.$root.msg)
+            }
         }
-    },
-    template: `
-        <div>
-            <button @click='getRoot'>子子组件</button>
-        </div>`,
-    methods: {
-        getRoot() {
-            console.log(this.msg)
-            console.log(this.$parent.msg)
-            console.log(this.$root.msg)
-        }
-    }
-})
-
+    })
+}
+export default plugins;
 // var Main = {
 //     data() {
 //         return {
